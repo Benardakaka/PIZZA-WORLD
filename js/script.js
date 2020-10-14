ad3e1dc68eed8cc134ffe88676ff9a0576aec9e6
Benardakaka <benardakaka484@gmail.com>
2020-10-14 10:02:57 +0300

    add image

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function subscribe() {
+    var Email = document.getElementById("Email").value;
+    if(Email){
+    alert("Hello,"+ Email + " we have received your message. Thank you for reaching out to us. *");
+} 
+}
