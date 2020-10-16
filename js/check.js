59914740d8906033d3f1093fd422dab2a8b85b63
Benardakaka <benardakaka484@gmail.com>
2020-10-16 17:40:04 +0300

    add js

--- js/check.js
+++ js/check.js
@@ -1,14 +1,15 @@
 let size, email;
 size = document.getElementById('#size').value;
 email = document.getElementById('#email').value;
+email = document.getElementById('#name').value;
 
 function subscribe() {
-    alert("Hello " + email + "Thank for reaching out");
-    console.log(email);
+    alert("Hello " + name + + email +" Thank for reaching out ");
+    console.log(name);
 }
 
 let validateInput = () => {
     if (size === "") {
-        console.log("Please specify the size");
+        console.log("Please specify the size ");
     }
 }
